c08e69f5285e81ecab6f0451822882e6e77d3186
adamw <adam@warski.org>
2021-03-19 16:14:35 +0100

    Initial

--- core/src/main/scala/context/Step7.scala
+++ core/src/main/scala/context/Step7.scala
@@ -5,7 +5,7 @@ import scala.concurrent.ExecutionContext
 object Step7 extends App:
   val x = "Generalisation of functions-as-values"
   val y = "Automatically inferred by the compiler"
-  val z = ""
+  val z = "Can be used to express environmental requirements"
 
   val docs = "https://dotty.epfl.ch/docs/reference/contextual/context-functions.html"
   val doobie = "https://tpolecat.github.io/doobie/docs/14-Managing-Connections.html"
